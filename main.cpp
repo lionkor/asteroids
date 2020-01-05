8e3286451674e133490b16cbfa3cea98453eabcc
Lion Kortlepel <development@kortlepel.com>
2020-01-05 21:21:12 +0100

    Add Bullets, shooting

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 #include <SFML/Graphics.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtx/rotate_vector.hpp>
@@ -7,14 +8,102 @@
 
 static inline constexpr double EPSILON = 0.0001;
 
-class Player
+class GameWindow : public sf::RenderWindow
+{
+public:
+    GameWindow(uint w, uint h, const char* title) : sf::RenderWindow(sf::VideoMode(w, h), title)
+    {
+        this->setFramerateLimit(30);
+    }
+    ~GameWindow() {}
+
+    auto width() const { return getSize().x; }
+    auto height() const { return getSize().y; }
+};
+
+template<typename _Callback_t>
+void handle_key_press(sf::Keyboard::Key key, _Callback_t fnc)
+{
+    if (sf::Keyboard::isKeyPressed(key))
+    {
+        fnc();
+    }
+}
+
+class PhysicsObject
+{
+public:
+    PhysicsObject(glm::vec2 pos, glm::vec2 vel, glm::vec2 acc, float size)
+        : m_pos(pos), m_vel(vel), m_acc(acc), m_size(size)
+    {
+    }
+
+    void physics_update()
+    {
+        m_vel += m_acc;
+        m_pos += m_vel;
+
+        m_acc = { 0, 0 };
+    }
+
+    void keep_in_bounds(GameWindow* window)
+    {
+        glm::vec2 mid = m_pos + glm::vec2(m_size / 2., m_size / 2.);
+        if (mid.x > window->width())
+        {
+            m_pos.x = 0 - m_size / 2.;
+        }
+        if (mid.y > window->height())
+        {
+            m_pos.y = 0 - m_size / 2.;
+        }
+
+        if (mid.x < 0)
+        {
+            m_pos.x = window->width() - m_size / 2.;
+        }
+        if (mid.y < 0)
+        {
+            m_pos.y = window->height() - m_size / 2.;
+        }
+    }
+
+protected:
+    glm::vec2 m_pos;
+    glm::vec2 m_vel;
+    glm::vec2 m_acc;
+    float     m_size;
+};
+
+class Bullet : public PhysicsObject
+{
+public:
+    static constexpr float BULLET_SPEED = 10.0f;
+
+    Bullet(const glm::vec2& pos, float dir, const glm::vec2& ship_vel)
+        : PhysicsObject(pos,
+                        (0.3f * ship_vel) +
+                            glm::rotate(glm::normalize((m_pos + glm::vec2(0, -1)) - m_pos) * BULLET_SPEED, dir),
+                        { 0, 0 }, 4.0f)
+    {
+    }
+
+    sf::VertexArray varray()
+    {
+        sf::VertexArray varr(sf::PrimitiveType::LineStrip, 4);
+        varr[0] = sf::Vertex(sf::Vector2f(m_pos.x + 0.0 * m_size, m_pos.y - 0.5 * m_size), sf::Color::White);
+        varr[1] = sf::Vertex(sf::Vector2f(m_pos.x - 0.5 * m_size, m_pos.y + 0.5 * m_size), sf::Color::White);
+        varr[2] = sf::Vertex(sf::Vector2f(m_pos.x + 0.5 * m_size, m_pos.y + 0.5 * m_size), sf::Color::White);
+        varr[3] = sf::Vertex(sf::Vector2f(m_pos.x + 0.0 * m_size, m_pos.y - 0.5 * m_size), sf::Color::White);
+        return varr;
+    }
+};
+
+class Player : public PhysicsObject
 {
 public:
     Player(uint x, uint y)
-        : m_size(20),
-          m_pos(x + m_size / 2., y + m_size / 2.),
-          m_vel(0, 0),
-          m_acc(0, 0),
+        : PhysicsObject({ x, y }, { 0, 0 }, { 0, 0 }, 20.0f),
           m_changed(true),
           m_acc_speed(0.6f),
           m_rotation(0.0f),
@@ -25,19 +114,39 @@ public:
 
     void update()
     {
-        m_vel += m_acc;
-        m_pos += m_vel;
-
-        // inertia dampening
-        m_vel *= 0.90f;
-        m_acc = { 0, 0 };
-
         m_changed = true;
 
         update_varray();
+        for (Bullet& bullet : m_bullets)
+        {
+            bullet.physics_update();
+        }
+
+        // inertia dampening
+        m_vel *= 0.90f;
     }
 
     void inform_about_mouse_pos(float x, float y) { m_mouse_pos = { x, y }; }
+
+
+    void shoot()
+    {
+        //                     max bullets
+        if (m_bullets.size() > 10)
+        {
+            m_bullets.pop_front();
+        }
+
+        m_bullets.push_back(Bullet(m_pos + glm::vec2(m_size / 2.0, m_size / 2.0), m_rotation, m_vel));
+    }
+
+    void draw_bullets(GameWindow* window)
+    {
+        for (Bullet& bullet : m_bullets)
+        {
+            window->draw(bullet.varray());
+        }
+    }
 
     enum class Direction
     {
@@ -75,6 +184,11 @@ public:
 
     sf::VertexArray varray() { return m_varray; }
 
+    const glm::vec2& position() const { return m_pos; }
+    void             set_position(const glm::vec2& pos) { m_pos = pos; }
+
+    std::deque<Bullet>& bullets() { return m_bullets; }
+
 protected:
     void update_varray()
     {
@@ -94,39 +208,15 @@ protected:
         }
     }
 
-    float           m_size;
-    glm::vec2       m_pos;
-    glm::vec2       m_vel;
-    glm::vec2       m_acc;
-    sf::VertexArray m_varray;
-    bool            m_changed;
-    float           m_acc_speed;
-    float           m_rotation;
-    glm::vec2       m_mouse_pos;
-    float           m_rotation_speed;
+    sf::VertexArray    m_varray;
+    bool               m_changed;
+    float              m_acc_speed;
+    float              m_rotation;
+    glm::vec2          m_mouse_pos;
+    float              m_rotation_speed;
+    std::deque<Bullet> m_bullets;
 };
 
-class GameWindow : public sf::RenderWindow
-{
-public:
-    GameWindow(uint w, uint h, const char* title) : sf::RenderWindow(sf::VideoMode(w, h), title)
-    {
-        this->setFramerateLimit(30);
-    }
-    ~GameWindow() {}
-
-    auto width() const { return getSize().x; }
-    auto height() const { return getSize().y; }
-};
-
-template<typename _Callback_t>
-void handle_key_press(sf::Keyboard::Key key, _Callback_t fnc)
-{
-    if (sf::Keyboard::isKeyPressed(key))
-    {
-        fnc();
-    }
-}
 
 int main(int, char**)
 {
@@ -145,11 +235,20 @@ int main(int, char**)
         handle_key_press(sf::Keyboard::D, [&]() { player.accelerate_in_direction(Player::Direction::Right); });
         handle_key_press(sf::Keyboard::Escape, [&]() { window.close(); });
 
+        player.physics_update();
         player.update();
 
+        player.keep_in_bounds(&window);
+        for (Bullet& bullet : player.bullets())
+        {
+            bullet.keep_in_bounds(&window);
+        }
+
+        player.draw_bullets(&window);
         window.draw(player.varray());
 
         window.display();
+
 
         while (window.pollEvent(event))
         {
@@ -157,6 +256,8 @@ int main(int, char**)
                 window.close();
             else if (event.type == sf::Event::MouseMoved)
                 player.inform_about_mouse_pos(event.mouseMove.x, event.mouseMove.y);
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+                player.shoot();
         }
     }
 
